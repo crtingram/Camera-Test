329aefa1dd35ebd925d007f30bbbd691a86e0463
Cory <crtingram@gmail.com>
2024-07-25 19:58:35 -0700

    Set up Item show/hide for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
@@ -6,13 +7,42 @@ public class PlayerController : MonoBehaviour {
 
     private CharacterController controller;
 
+    private GameObject Item_Axe, Item_Pickaxe, Item_Sword;
+
     void Start() {
+        Item_Axe = transform.GetChild(1).gameObject;
+        Item_Pickaxe = transform.GetChild(2).gameObject;
+        Item_Sword = transform.GetChild(3).gameObject;
         controller = gameObject.GetComponent<CharacterController>();
     }
 
     void FixedUpdate() {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         controller.Move(move * Time.deltaTime * walkSpeed);
+    }
+
+    void Update() {
+        UpdateItem();
+    }
+
+    void UpdateItem() {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            Item_Axe.SetActive(!Item_Axe.activeSelf);
+            Item_Pickaxe.SetActive(false);
+            Item_Sword.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            Item_Axe.SetActive(false);
+            Item_Pickaxe.SetActive(!Item_Pickaxe.activeSelf);
+            Item_Sword.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            Item_Axe.SetActive(false);
+            Item_Pickaxe.SetActive(false);
+            Item_Sword.SetActive(!Item_Sword.activeSelf);
+        }
     }
 
 }
