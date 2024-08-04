b17496cab574f0c99499578067710b4e31bc8f31
Cory <crtingram@gmail.com>
2024-08-03 22:05:41 -0700

    Random BS

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Collider))]
 public class PlayerController : MonoBehaviour {
 
     private CharacterController controller;
