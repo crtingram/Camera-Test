1fe999dd73c8326b2b25fe5ab462b869a8268043
Cory <crtingram@gmail.com>
2024-08-08 21:30:26 -0700

    Some Editor Generation scripts (needs alot of work).

--- Assets/Editor/GeneratorEditor.cs
+++ Assets/Editor/GeneratorEditor.cs
@@ -4,13 +4,13 @@ using UnityEditor;
 [CustomEditor(typeof(GeneratorScript))]
 public class GeneratorEditor : Editor {
 
-    public GameObject test;
+    public GameObject tree;
 
     public override void OnInspectorGUI() {
         GeneratorScript mapGen = (GeneratorScript)target;
 
-        if (GUILayout.Button("Generate")) {
-            mapGen.GenerateLine();
+        if (GUILayout.Button("Generate Tree(s)")) {
+            mapGen.GenerateLine(tree);
         }
     }
 }
--- Assets/Scripts/GeneratorScript.cs
+++ Assets/Scripts/GeneratorScript.cs
@@ -1,15 +1,12 @@
 using UnityEngine;
 
-[System.Serializable]
 public class GeneratorScript : MonoBehaviour {
 
     public int lengthX = 1;
-    public GameObject spam1;
 
-    public void GenerateLine() {
-        print("Test'");
+    public void GenerateLine(GameObject go) {
         for (int i = 0; i < lengthX; i++) {
-
+            Instantiate(go);
         }
     }
 
